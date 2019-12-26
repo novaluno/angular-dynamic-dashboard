726ead99980b61bdd89cddd83e8fbb2a44c7c20c
Dennis Moon <dmoon.comfycoder@gmail.com>
2019-12-26 10:53:17 -0600

    Added common modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'Welcome!'
+    }
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+    data: {
+      title: 'Error'
+    }
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    data: {
+      title: 'Not Found'
+    }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
