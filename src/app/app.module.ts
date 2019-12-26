ea33e5c7fdd571d28a7ec193f98ad58c517c3ebd
Dennis Moon <dmoon.comfycoder@gmail.com>
2019-12-26 12:20:59 -0600

    Added kendo ui components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +30,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    DropDownsModule,
+    GridModule,
+    PDFExportModule
   ],
   providers: [],
   bootstrap: [AppComponent]
