1315ba19e518b77ca94905919a4208766715719e
Dennis Moon <dmoon.comfycoder@gmail.com>
2019-12-26 12:58:43 -0600

    Added widget host directive

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,10 +5,11 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { NewsUpdatesComponent } from './news-updates/news-updates.component';
 import { DataUpdatesComponent } from './data-updates/data-updates.component';
+import { WidgetHostDirective } from './widget-host.directive';
 
 
 @NgModule({
-  declarations: [DashboardComponent, NewsUpdatesComponent, DataUpdatesComponent],
+  declarations: [DashboardComponent, NewsUpdatesComponent, DataUpdatesComponent, WidgetHostDirective],
   imports: [
     CommonModule,
     DashboardRoutingModule
